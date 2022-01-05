4cc0e0740053321a664ba2bbc305a9c016f7c288
Denzel <dbartolaba123@gmail.com>
2022-01-05 14:26:55 -0800

    todo stuff

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,16 @@
 export function renderTodo(todo) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
     // create a div and a p tag
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
+    div.classList.add(todo.complete ? 'complete' : 'incomplete');
+    div.classList.add('todo');
     // add the 'todo' css class no matter what
-
+    p.textContent = todo.todo;
     // put the todo's text into the p tag
 
     // append stuff
-
+    div.append(p);
     // return the div
+    return div;
 }
